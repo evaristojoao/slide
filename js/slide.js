693c9e73d90dc19b549e1166350cac33d787609a
João Eugénio <joaoeugenio20@gmail.com>
2025-05-06 19:13:47 +0100

    Touchevents added

--- js/slide.js
+++ js/slide.js
@@ -2,41 +2,52 @@ export default class Slide {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide);
     this.wrapper = document.querySelector(wrapper);
-    this.dist = { finalPosition: 0,  startX: 0, movement: 0 };
+    this.dist = { finalPosition: 0,  startX: 0, movement: 0 }; // Armazena os dados da movimentação
   }
 
-  moveSlide(distX) {
+  moveSlide(distX) { // Move o slide horizontalmente (translate3d) para a posição distX.
     this.dist.movePosition = distX;
-    this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
+    this.slide.style.transform = `translate3d(${distX}px, 0, 0)`; 
   }
  
-  updatePosition(clientX) {
+  updatePosition(clientX) { // Calcula quanto o slide deve se mover, Multiplica por 1.6 para dar um efeito de aceleração, Retorna a nova posição do slide baseada no movimento do mouse.
     this.dist.movement = (this.dist.startX - clientX) * 1.6;
     return this.dist.finalPosition - this.dist.movement;
   }
 
-  onStart(event) {
-    event.preventDefault();
-    this.dist.startX = event.clientX;
-    this.wrapper.addEventListener('mousemove', this.onMove);
+  onStart(event) { // Inicia o movimento, Salva a posição inicial do mouse (startX), Começa a escutar o movimento do mouse.
+    let movetype;
+    if (event.type === 'mousedown') {
+        event.preventDefault();
+        this.dist.startX = event.clientX;
+        movetype = 'mousemove';
+    } else {
+        this.dist.startX = event.changedTouches[0].clientX;
+        movetype = 'touchmove';
+    }
+    this.wrapper.addEventListener(movetype, this.onMove);
   }
 
-  onMove(event) {
-    const finalPosition = this.updatePosition(event.clientX);
+  onMove(event) { // Enquanto o mouse se move, calcula a nova posição e move o slide.
+    const pointerPosition = (event.type === 'mousemove') ? event.clientX : event.changedTouches[0].clientX;
+    const finalPosition = this.updatePosition(pointerPosition);
     this.moveSlide(finalPosition);
   }
 
-  onEnd(event) {
-    this.wrapper.removeEventListener('mousemove', this.onMove);
+  onEnd(event) { // Pára o movimento, Define finalPosition como a última posição do slide.
+    const movetype = (event.type === 'mouseup') ? 'mousemove' : 'touchmove';
+    this.wrapper.removeEventListener(movetype, this.onMove);
     this.dist.finalPosition = this.dist.movePosition;
   }
 
   addSlideEvents() {
     this.wrapper.addEventListener('mousedown', this.onStart);
+    this.wrapper.addEventListener('touchstart', this.onStart);
     this.wrapper.addEventListener('mouseup', this.onEnd);
+    this.wrapper.addEventListener('touchend', this.onEnd);
   }
 
-  bindEvents() {
+  bindEvents() { // 
     this.onStart = this.onStart.bind(this); // usamos o bind para alterar o ".this" quando clicado nas imagens. antes trazia o .slide-wrapper, Com o bind traz o .slide (referencia ao objeto)
     this.onMove = this.onMove.bind(this); 
     this.onEnd = this.onEnd.bind(this); 
