7c064b398991eb4baac04769edf165cd83816f1c
João Eugénio <joaoeugenio20@gmail.com>
2025-05-13 09:24:31 +0100

    Add controls and resize logic

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
-import Slide from './slide.js';
+import { SlideNav } from './slide.js';
+// 1º - Cria uma nova instância do objeto Slide, passando os seletores do slide e do wrapper
+const slide = new SlideNav('.slide', '.slide-wrapper');
 
-const slide = new Slide('.slide', '.slide-wrapper');
 slide.init();
-slide.changeSlide(3);
-slide.activePrevSlide();
+slide.addArrow('.prev', '.next');
+slide.addControl();
+
+
+// slide.changeSlide(3);
+// slide.activePrevSlide();
